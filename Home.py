d7a421ee630c53d9c2769eb59b59f79abfac9fa1
Yuvraj-s-Maxval <yuvraj.s@maxval.com>
2024-10-23 16:09:10 +0530

    Updated the path for json file for firebase authenticaion

--- Home.py
+++ Home.py
@@ -9,22 +9,8 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Correctly format the private key with newlines
-firebase_creds = {
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
-}
-
 # Initialize Firebase
-cred = credentials.Certificate(firebase_creds)
+cred = credentials.Certificate("fin-rag-firebase-adminsdk-un38r-81d28e871c.json")
 
 # Initialize Firebase app only if not already initialized
 try:
